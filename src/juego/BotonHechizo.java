080cdca80da84e486171b7acf0b67996e125811f
Duarte Nazareno <duartenaza@gmail.com>
2025-05-13 15:24:24 -0300

    se agrego la clase BotonHechizo

---
+++ src/juego/BotonHechizo.java
@@ -0,0 +1,25 @@
+package juego;
+
+import java.awt.Color;
+import entorno.Entorno;
+
+public class BotonHechizo {
+	private String nombre;
+	private int y;
+	private int costo;
+
+	public BotonHechizo(String nombre, int y, int costo) {
+		this.nombre = nombre;
+		this.y = y;
+		this.costo = costo;
+	}
+
+	public void dibujar(Entorno entorno, int offsetX, boolean seleccionado) {
+		Color color = seleccionado ? Color.ORANGE : Color.WHITE;
+		entorno.dibujarRectangulo(offsetX + 100, y, 180, 40, 0, color);
+		entorno.cambiarFont("Arial", 14, Color.BLACK);
+		entorno.escribirTexto(nombre + " (" + costo + ")", offsetX + 30, y + 5);
+	}
+
+	public boolean fueClickeado(int mouseX, int mouseY, int offsetX) {
+		return mouseX >= offsetX && mouseX <= offsetX + 200 && mouseY >= y - 20 && mouseY <= y + 20;
